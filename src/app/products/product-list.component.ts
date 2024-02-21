bf0a309e801747b3f99468f4c69fa5c338299b37
Pilar Solis <pilar.sm02@gmail.com>
2024-02-21 08:01:12 +0100

    search products filter added

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,18 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
-    listFilter: string = "cart";
+    
+    private _listFilter: string = '';
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      console.log('In setter: ', value);
+      this.filteredProducts = this.performFilter(value);
+    }
+
+    filteredProducts : IProduct[] = [];
     products: IProduct[] = [
         {
             "productId": 2,
@@ -36,11 +47,18 @@ export class ProductListComponent implements OnInit {
           }
     ];
 
+
+    performFilter(filteredBy: string): IProduct[]{
+      filteredBy = filteredBy.toLocaleLowerCase(); //for case-insensitive comparison
+      return this.products.filter((product : IProduct) => 
+      product.productName.toLocaleLowerCase().includes(filteredBy));
+    }
+
     toggleImage(): void {
       this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-      console.log('In OnInit');
+      this.listFilter = 'cart';
     }
 }
