aae29bc320fdada6577c53be69cd32ccfccd306e
Pilar Solis <pilar.sm02@gmail.com>
2024-03-02 22:11:31 +0100

    Exception handling (for http request)

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Observable, catchError, tap, throwError } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,26 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
     getProducts(): Observable<IProduct[]> {
-       return this.http.get<IProduct[]>(this.productUrl); 
+       return this.http.get<IProduct[]>(this.productUrl).pipe(
+        tap(data => console.log('All', JSON.stringify(data))),
+        catchError(this.handleError)
+       ); 
+    }
+
+    private handleError(err: HttpErrorResponse) {
+      //in a real world app, I may send the server to some remote logging infrastructure instead of just logging it to the console
+    
+      let errorMessage = '';
+      if (err.error instanceof ErrorEvent) {
+        //a client-side or netword error occurred.Handle it accordingly
+        errorMessage = `An error occurred: ${err.error.message}`;
+      } else {
+        //The back-end returned and unsuccessful response code
+        //the response body may contain clues as to what went wrong
+        errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      }
+      console.error(errorMessage);
+      return throwError(() => errorMessage);
     }
 
 }
