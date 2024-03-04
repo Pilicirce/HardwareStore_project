7595080db667b1ad4b744c0250e5af9845d5f417
Pilar Solis <pilar.sm02@gmail.com>
2024-03-04 08:34:23 +0100

    subcribing to an Observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component ({
     selector: 'pm-products',
@@ -8,11 +9,13 @@ import { ProductService } from "./product.service";
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number =2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
     
     private _listFilter: string = '';
     get listFilter(): string {
@@ -41,8 +44,19 @@ constructor(private productService: ProductService){}
     }
 
     ngOnInit(): void {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products; //para que aparezcan solo los products filtrados
+      this.sub = this.productService.getProducts().subscribe({
+        next: products => {
+          this.products = products
+          this.filteredProducts = this.products; //para que aparezcan solo los products filtrados
+        },
+        error: err => this.errorMessage = err
+ 
+      });
+       
+    }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
     }
 
     onRatingClicked (message: string): void {
