72a7021fdf09a24420d30f8c62c460f8a14081a8
Pilar Solis <pilar.sm02@gmail.com>
2024-03-16 11:39:41 +0100

    handling null and undefinded (hardcoding for now)

--- src/app/products/products-detail.component.ts
+++ src/app/products/products-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './products-detail.component.html',
@@ -7,12 +8,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductsDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  product: IProduct | undefined;
 
   constructor(private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `:${id}`;
+    this.product = {
+    "productId": id,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2021",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "assets/images/leaf_rake.png"
+    };
   }
 
 }
