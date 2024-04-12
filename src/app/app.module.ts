6777476be3c2f7968f2415403033c0bbd483f3d8
Pilar Solis <pilar.sm02@gmail.com>
2024-04-12 08:09:43 +0200

    imports/exports corrected to the new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductsDetailComponent } from './products/products-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,  
-    ProductListComponent,
-    ConvertToSpacesPipe, 
-    StarComponent,
-    ProductsDetailComponent
+    WelcomeComponent  
+    
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot ([
-      {path: 'products', component: ProductListComponent},
-      {
-        path: 'products/:id', 
-        canActivate: [ProductDetailGuard],
-        component: ProductsDetailComponent
-      },
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list.component';
+import { ProductsDetailComponent } from './products-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductsDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {path: 'products', component: ProductListComponent},
+      {
+        path: 'products/:id', 
+        canActivate: [ProductDetailGuard],
+        component: ProductsDetailComponent
+      },
+    ])
   ]
 })
 export class ProductModule { }
