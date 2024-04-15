62039f441b77703e1689ea75453ee9a71a73dc2e
Pilar Solis <pilar.sm02@gmail.com>
2024-04-15 07:40:32 +0200

    create shared module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,6 +7,7 @@ import { StarComponent } from '../shared/star.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -27,7 +28,8 @@ import { ProductDetailGuard } from './product-detail.guard';
         canActivate: [ProductDetailGuard],
         component: ProductsDetailComponent
       },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
